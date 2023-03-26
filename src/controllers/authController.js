7017ebd2eb0b4ac13ac692b48e3c9cc15e79f009
ekarinT <ekarin.thin@outlook.com>
2023-03-26 09:34:48 +0700

    add error alert

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,9 +3,12 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const AppError = require('../utils/appError');
-const { User } = require('../models/User');
+const { User } = require('../models');
 
-const genToken = (payload) => jwt.sign(payload,);
+const genToken = (payload) =>
+  jwt.sign(payload, process.env.JWT_SECRET_KEY || 'private_key', {
+    expiresIn: process.env.JWT_EXPIRES || '1d'
+  });
 
 exports.register = async (req, res, next) => {
   try {
@@ -39,6 +42,9 @@ exports.register = async (req, res, next) => {
       mobile: isMobile ? emailOrMobile : null,
       password: hashedPassword
     });
+
+    const token = genToken({ id: user.id });
+    res.status(201).json({ token });
   } catch (err) {
     next(err);
   }
