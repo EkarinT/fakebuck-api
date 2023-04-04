3e6cc14c972670090dd20948d44f1d2fd5637150
ekarinT <ekarin.thin@outlook.com>
2023-04-04 10:05:00 +0700

    feature login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,12 @@
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { Op } = require('sequelize');
 
 const AppError = require('../utils/appError');
 const { User } = require('../models');
 
-const genToken = (payload) =>
+const genToken = payload =>
   jwt.sign(payload, process.env.JWT_SECRET_KEY || 'private_key', {
     expiresIn: process.env.JWT_EXPIRES || '1d'
   });
@@ -45,6 +46,40 @@ exports.register = async (req, res, next) => {
 
     const token = genToken({ id: user.id });
     res.status(201).json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { emailOrMobile, password } = req.body;
+
+    if (
+      !emailOrMobile ||
+      typeof emailOrMobile !== 'string' ||
+      typeof password !== 'string'
+    ) {
+      throw new AppError('email or mobile or password is invalid', 400);
+    }
+
+    const user = await User.findOne({
+      where: {
+        [Op.or]: [{ email: emailOrMobile }, { mobile: emailOrMobile }]
+      }
+    });
+
+    if (!user) {
+      throw new AppError('email or mobile or password is invalid', 400);
+    }
+
+    const isCorrect = await bcrypt.compare(password, user.password);
+    if (!isCorrect) {
+      throw new AppError('email or mobile or password is invalid', 400);
+    }
+
+    const token = genToken({ id: user.id });
+    res.status(200).json({ token });
   } catch (err) {
     next(err);
   }
